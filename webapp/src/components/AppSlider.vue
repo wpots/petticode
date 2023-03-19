25b77ab2b8f6002ab5cf21afc57b8abdadc76efd
wpots <12676504+wpots@users.noreply.github.com>
2023-03-19 15:32:24 +0100

    install vue

---
+++ webapp/src/components/AppSlider.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import SliderItem from './SliderItem.vue'
+</script>
+
+<template>
+  <div
+    class="tagline skrollable skrollable-between"
+    data-0="font-size:15px;"
+    data-100="font-size:20px;opacity:1"
+    data-200="opacity:0;"
+    style="font-size: 15px; opacity: 1"
+  >
+    <h2>ˈpɛtɪkəʊt/</h2>
+    <div class="definitions" data-loop>
+      <p class="">
+        <span>Informal:</span> used to denote female control of something regarded as more commonly
+        dominated by men.
+      </p>
+      <p class=""><span>Usage note:</span> a humorous or mildly disparaging name for a woman.</p>
+      <p class="focus">
+        <span>As modifier:</span> of, relating to, or controlled by women; female; feminine.
+      </p>
+    </div>
+  </div>
+</template>
---
+++ webapp/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const video = ref<HTMLInputElement | null>(null)
+onMounted(() => {
+  if (video.value) {
+    video.value.play()
+  }
+})
+</script>
+
+<template>
+  <main>
+    <video ref="video" poster="@/assets/petticodestill-3.png" muted>
+      <source src="@/assets/petticode-teaser.webm#t=0,1.4" type="video/webm" />
+    </video>
+  </main>
+</template>
+<style lang="scss">
+video {
+  width: 100vw;
+  height: 100vh;
+  object-fit: contain;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
